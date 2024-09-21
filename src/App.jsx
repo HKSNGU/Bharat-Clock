0fa31a6a5be75f08f7275290d805879959064c79
Himanshu Sah <hksngu@gmail.com>
2024-09-21 23:50:20 +0530

    Description Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Desc from "./components/Desc";
 
 function App() {
   return (
     <>
       <Header />
+      <Desc />
     </>
   );
 }
---
+++ src/components/Desc.jsx
@@ -0,0 +1,9 @@
+function Desc() {
+  return (
+    <>
+      <p>This is the clock that shows time in bharat all the times</p>
+    </>
+  );
+}
+
+export default Desc;
